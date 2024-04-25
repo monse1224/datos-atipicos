c710bf37dd893aab4211e55bd47b739d4f00a56d
monse1224 <A01709064@tec.mx>
2024-04-24 18:40:48 -0600

    cambios

--- outliers.py
+++ outliers.py
@@ -11,19 +11,19 @@ valores_nulos=df.isnull().sum()
 #print(valores_nulos)
 
 fig = plt.figure(figsize =(7, 3))
-plt.hist(x=df["ventas_precios_corrientes"], color='blue', rwidth=0.50)
-plt.title('Histograma de ventas_precios_corrientes')
+plt.hist(x=df["ventas_precios_corrientes"], color='red', rwidth=0.50)
+plt.title('Histograma de ventas_precios_corrientes con outliers')
 plt.xlabel('ventas_precios_corrientes')
 plt.ylabel('Frecuencia')
 #plt.show()
 
 fig = plt.figure(figsize =(5, 3))
 plt.boxplot(df["ventas_precios_corrientes"]) 
-plt.title("Outliers de ventas_precios_corriente")
+plt.title("Outliers de ventas_precios_corriente con outliers")
 #plt.show()
 
-y=df('ventas_precios_corrientes')
-#print(y)
+y=df['ventas_precios_corrientes']
+print(y)
 
 percentile25=y.quantile(0.25) #Q1
 percentile75=y.quantile(0.75) #Q3
@@ -37,4 +37,18 @@ Limite_Inferior_iqr= percentile25 - 1.5*iqr
 print("Limite superior permitido", Limite_Superior_iqr)
 print("Limite inferior permitido", Limite_Inferior_iqr)
 
-     
+#Obtenemos datos limpios
+data_clean_iqr= df[(y<=Limite_Superior_iqr)&(y>=Limite_Inferior_iqr)]
+print(data_clean_iqr)
+
+fig = plt.figure(figsize =(5, 3))
+plt.boxplot(data_clean_iqr["ventas_precios_corrientes"]) 
+plt.title("Outliers de ventas_precios_corriente")
+plt.show()
+
+fig = plt.figure(figsize =(7, 3))
+plt.hist(x=data_clean_iqr["ventas_precios_corrientes"], color='red', rwidth=0.50)
+plt.title('Histograma de ventas_precios_corrientes sin outliers')
+plt.xlabel('ventas_precios_corrientes')
+plt.ylabel('Frecuencia')
+#plt.show()
