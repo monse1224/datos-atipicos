07330d5a960b017db23dafb375cf43b8024f3bd4
monse1224 <A01709064@tec.mx>
2024-04-29 20:46:36 -0600

    Cambios

--- gastos_costos.py
+++ gastos_costos.py
@@ -3,3 +3,138 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 df2= pd.read_excel('gastos_costos_20_23.xlsx', index_col=0)
+
+valores_nulos=df2.isnull().sum()
+#print(valores_nulos)
+
+fig = plt.figure(figsize =(7, 3))
+plt.hist(x=df2["IVA"], color='red', rwidth=0.50)
+plt.title('Histograma de IVA con outliers')
+plt.xlabel('IVA')
+plt.ylabel('Frecuencia')
+#plt.show()
+
+fig = plt.figure(figsize =(5, 3))
+plt.boxplot(df2["IVA"]) 
+plt.title("IVA con outliers")
+#plt.show()
+
+y=df2['IVA']
+#print(y)
+
+#percentile25=y.quantile(0.25) #Q1
+#percentile75=y.quantile(0.75) #Q3
+#print(percentile25)
+#print(percentile75)
+#iqr= percentile75 - percentile25
+#print(iqr)
+
+#Limite_Superior_iqr= percentile75 + 1.5*iqr
+#Limite_Inferior_iqr= percentile25 - 1.5*iqr
+#print("Limite superior permitido", Limite_Superior_iqr)
+#print("Limite inferior permitido", Limite_Inferior_iqr)
+
+#Obtenemos datos limpios
+#data_clean_iqr= df2[(y<=Limite_Superior_iqr)&(y>=Limite_Inferior_iqr)]
+#print(data_clean_iqr)
+
+fig = plt.figure(figsize =(5, 3))
+#plt.boxplot(data_clean_iqr["IVA"]) 
+plt.title("Outliers de IVA")
+#plt.show()
+
+fig = plt.figure(figsize =(7, 3))
+#plt.hist(x=data_clean_iqr["IVA"], color='red', rwidth=0.50)
+plt.title('Histograma de IVA sin outliers')
+plt.xlabel('IVA')
+plt.ylabel('Frecuencia')
+#plt.show()
+
+#Método con desviación estándar
+#data_clean_iqr["IVA"].to_csv('IVA.csv')
+
+#y=df2["IVA"]
+Limite_Superior_dev_std= y.mean() + 3*y.std()
+Limite_Inferior_dev_std= y.mean() - 3*y.std()
+#print("Limite superior permitido usando desv estandar", Limite_Superior_dev_std)
+#print("Limite inferior permitido usando desv estandar", Limite_Inferior_dev_std)
+
+#Obtenemos datos limpios
+data_clean_dev_std= df2[(y<=Limite_Superior_dev_std)&(y>=Limite_Inferior_dev_std)]
+#print(data_clean_dev_std)
+
+fig = plt.figure(figsize =(7, 3))
+#plt.hist(x=data_clean_iqr["IVA"], color='red', rwidth=0.50)
+plt.title('Histograma de IVA sin outliers- desv std')
+plt.xlabel('IVA')
+plt.ylabel('Frecuencia')
+#plt.show()
+
+#COLUMNA 2: IMPORTE
+fig2 = plt.figure(figsize =(7, 3))
+plt.hist(x=df2["IMPORTE"], color='red', rwidth=0.50)
+plt.title('Histograma de IMPORTE con outliers')
+plt.xlabel('IMPORTE')
+plt.ylabel('Frecuencia')
+#plt.show()
+
+fig2 = plt.figure(figsize =(5, 3))
+plt.boxplot(df2["IMPORTE"]) 
+plt.title("IMPORTE con outliers")
+plt.show()
+
+y2=df2['IMPORTE']
+print(y2)
+
+percentile25=y2.quantile(0.25) #Q1
+percentile75=y2.quantile(0.75) #Q3
+print(percentile25)
+print(percentile75)
+iqr2= percentile75 - percentile25
+print(iqr2)
+
+Limite_Superior_iqr2= percentile75 + 1.5*iqr2
+Limite_Inferior_iqr2= percentile25 - 1.5*iqr2
+print("Limite superior permitido", Limite_Superior_iqr2)
+print("Limite inferior permitido", Limite_Inferior_iqr2)
+
+#Obtenemos datos limpios
+data_clean_iqr2= df2[(y<=Limite_Superior_iqr2)&(y>=Limite_Inferior_iqr2)]
+print(data_clean_iqr2)
+
+fig2 = plt.figure(figsize =(5, 3))
+plt.boxplot(data_clean_iqr2["IMPORTE"]) 
+plt.title("Outliers de IMPORTE")
+plt.show()
+
+fig2 = plt.figure(figsize =(7, 3))
+plt.hist(x=data_clean_iqr2["IMPORTE"], color='red', rwidth=0.50)
+plt.title('Histograma de IMPORTE sin outliers')
+plt.xlabel('IMPORTE')
+plt.ylabel('Frecuencia')
+plt.show()
+
+#Método con desviación estándar
+data_clean_iqr2["IMPORTE"].to_csv('IMPORTE.csv')
+
+y2=df2["IMPORTE"]
+Limite_Superior_dev_std2= y2.mean() + 3*y.std()
+Limite_Inferior_dev_std2= y2.mean() - 3*y.std()
+print("Limite superior permitido usando desv estandar", Limite_Superior_dev_std2)
+print("Limite inferior permitido usando desv estandar", Limite_Inferior_dev_std2)
+
+#Obtenemos datos limpios
+data_clean_dev_std2= df2[(y2<=Limite_Superior_dev_std2)&(y2>=Limite_Inferior_dev_std2)]
+print(data_clean_dev_std2)
+
+fig2 = plt.figure(figsize =(7, 3))
+plt.hist(x=data_clean_iqr2["IMPORTE"], color='red', rwidth=0.50)
+plt.title('Histograma de IMPORTE sin outliers- desv std')
+plt.xlabel('IMPORTE')
+plt.ylabel('Frecuencia')
+plt.show()
+
+
+
+
+
